219abfb48a85b656294abd2894c2b39f24532840
Nuttapol Runsewa <nrunsewa@gmail.com>
2025-03-03 17:11:12 +0700

    Uncomment to pass first stage

--- app/main.go
+++ app/main.go
@@ -10,8 +10,8 @@ import (
 var _ = fmt.Fprint
 
 func main() {
-	// Uncomment this block to pass the first stage
-	// fmt.Fprint(os.Stdout, "$ ")
+	//Uncomment this block to pass the first stage
+	fmt.Fprint(os.Stdout, "$ ")
 
 	// Wait for user input
 	bufio.NewReader(os.Stdin).ReadString('\n')
